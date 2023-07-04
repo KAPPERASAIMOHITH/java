13c5c56fe97c07b2a6386f5766e2a30ea172a535
KAPPERASAIMOHITH <113877277+KAPPERASAIMOHITH@users.noreply.github.com>
2023-07-04 18:58:36 +0530

    Create sumoftwonum.java

---
+++ sumoftwonum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class sum
+{
+public static void main(String[] args)
+{
+System.out.println("enter the two numbers");
+Scanner val=new Scanner(System.in);
+int a=val.nextInt();
+int b=val.nextInt();
+int c=a+b;
+System.out.println("sum is "+c);
+}}

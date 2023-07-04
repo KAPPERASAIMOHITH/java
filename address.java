3abee00de1c17e06885981fb3aaf9d86cb3a6f4d
KAPPERASAIMOHITH <113877277+KAPPERASAIMOHITH@users.noreply.github.com>
2023-07-04 20:00:32 +0530

    Create address.java

---
+++ address.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class address{
+public static void main(String[] args){
+Scanner val=new Scanner(System.in);
+System.out.println("enter the name:");
+String a=val.nextLine();
+System.out.println("enter the father name:");
+String b=val.nextLine();
+System.out.println("enter the dno:");
+String c=val.nextLine();
+System.out.println("enter the streetname:");
+String d=val.nextLine();
+System.out.println("enter the areaname:");
+String e=val.nextLine();
+System.out.println("enter the cityname:");
+String f=val.nextLine();
+System.out.println("enter the pincode:");
+int g=val.nextInt();
+System.out.println("name:"+a);
+System.out.println("father name:"+b);
+System.out.println("dno:"+c);
+System.out.println("streetname:"+d);
+System.out.println("areaname:"+e);
+System.out.println("cityname:"+f);
+System.out.println("pincode:"+g);
+}}
+
+
